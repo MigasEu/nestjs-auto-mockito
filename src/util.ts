41f3946b97828db5b7268f2200b15b607dbd1ce9
Miguel Castro <miguel.castro@nearseatech.io>
2022-01-27 19:01:37 +0000

    fix: prettier check

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,6 @@
 import { Mocker } from 'ts-mockito/lib/Mock';
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 export function betterMock<T>(clazz?: (new (...args: any[]) => T) | (Function & { prototype: T })): T {
   const mocker = new Mocker(clazz);
   mocker['excludedPropertyNames'] = ['hasOwnProperty', 'then'];
